40744b18c390e8164c01e22cc4e18ab4877e6785
hemanthkumar <hemanthkumarr21cse@srishakthi.ac.in>
2025-06-26 13:52:26 +0530

    Added job type to the job posting form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Routes, Route} from "react-router-dom";
-// import Home from './Client/pages/Home';
 import Home from './pages/Home.js';
 const App = () => {
   return (
