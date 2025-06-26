4ed49de5af2d077503be3b782402fa7681379596
hemanthkumar <hemanthkumarr21cse@srishakthi.ac.in>
2025-06-26 13:29:05 +0530

    Update cors in backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
-
 const mongoose = require('mongoose');
-const path = require('path');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,27 +7,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://jobportal-3-1gq0.onrender.com',  // Frontend URL
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 // Import routes
 const adminRoutes = require('./routes/adminRoutes.js');
-
 app.use('/admin', adminRoutes);
-// Use routes with base path '/admin'
-
-// // Serve static files from React build
-// app.use(express.static(path.join(__dirname, '../client/build')));
-
-// app.get(/^\/(?!admin|api).*/, (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-// });
-
 
 // Start server
 app.listen(PORT, () => {
