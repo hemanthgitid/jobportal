215355e46a4d9cae9e2062ab6e8c97557f4e8401
hemanthkumar <hemanthkumarr21cse@srishakthi.ac.in>
2025-07-04 09:17:27 +0530

    initial

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import navbar from '../css/Navbar.module.css';
-import logo from '../assets/logo1.png'
-const Navbar = ({createjob}) => {
-  return (
-    <div>
-        <nav className={navbar.navbar_head}>
-            <ul>
-                <img src={logo} width={70} height={50} alt="company logo" />
-                <li>Home</li>
-                <li>Find Jobs</li>
-                <li>Find Talents</li>
-                <li>About us</li>
-                <li>Testimonials</li>
-                <li><button onClick={createjob}>Create jobs</button></li>
-            </ul>
-        </nav>
-    </div>
-  )
-}
+import React, { useState } from 'react';
+import styles from '../css/Navbar.module.css';
+import logo from '../assets/logo1.png';
 
-export default Navbar
+const Navbar = ({ createjob }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Company Logo" className={styles.logo} />
+      </div>
+
+      <button className={styles.menuButton} onClick={toggleNav} aria-label="Menu">
+        {isOpen ? '✖' : '☰'}
+      </button>
+
+      <ul className={`${styles.navLinks} ${isOpen ? styles.showMenu : ''}`}>
+        <li>Home</li>
+        <li>Find Jobs</li>
+        <li>Find Talents</li>
+        <li>About Us</li>
+        <li>Testimonials</li>
+        <li>
+          <button onClick={createjob} className={styles.createButton}>Create Jobs</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
